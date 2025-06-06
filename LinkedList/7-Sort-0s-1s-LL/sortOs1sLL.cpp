67e3384c68b6039b1cb432a42d2603538169311d
Pratim <hpro373@gmail.com>
2025-06-06 23:34:40 +0530

    add optimal sol

--- LinkedList/7-Sort-0s-1s-LL/sortOs1sLL.cpp
+++ LinkedList/7-Sort-0s-1s-LL/sortOs1sLL.cpp
@@ -1,4 +1,5 @@
 #include<iostream>  
+#include <vector>   
 using namespace std;
 
 class Node {
@@ -44,14 +45,77 @@ Node* bruteForce(Node* head){
     //TC - > O(2N) note: optmise this to O(1);
     //SC - > O(1)
 
-}
+    int cnt0 = 0, cnt1 = 0 , cnt2=0;
+    Node* temp = head;
+    while(temp != NULL){
+        if(temp->data == 0){
+            cnt0++;
+        }else if(temp->data == 1){
+            cnt1++;
+        }else if(temp->data == 2){
+            cnt2++;
+        }
+        temp = temp->next;
+    }
+    temp = head;
+    while(temp != NULL){
+        if(cnt0 !=0){
+            temp->data = 0;
+            cnt0--;
+        }else if(cnt1 != 0){
+             temp->data = 1;
+             cnt1--;
+        }else if(cnt2 != 0){
+            temp->data = 2;
+            cnt2--;
+        }
+        temp = temp->next;
+    }
+    return head;
 
+}
+Node* optimal(Node* head){
+    if( head == NULL || head->next == NULL) return head;
+    Node* zeroHead = new Node(-1);
+    Node* oneHead = new Node(-1);
+    Node* twoHead = new Node(-1);
+    
+    Node* zero = zeroHead;
+    Node* one = oneHead;
+    Node* two = twoHead;
+    Node* temp = head;
+    
+    while(temp){
+        if(temp->data == 0){
+            zero->next = temp;
+            zero = zero->next;
+        }else if(temp->data == 1){
+            one->next = temp;
+            one = one->next;
+        }else if(temp->data == 2){
+            two->next = temp;
+            two = two->next;
+        }
+        temp = temp->next;
+    }
+    zero->next = (oneHead->next) ? (oneHead->next) :(twoHead->next);
+    one->next = twoHead->next;
+    two->next = NULL;
+    Node* newHead = zeroHead->next;
+    delete zeroHead;
+    delete oneHead;
+    delete twoHead;
+    return newHead;
+
+}
 int main()
 {
     vector<int> arr = {0,1,0,1,2,1,2};
    //Output: [0,0,1,1,1,2,2]
     Node* head = convetArrayToDoubleLL(arr);
- //  print(head);
+    // head = bruteForce(head);
+        head = optimal(head);
+    print(head);
 
   
     
