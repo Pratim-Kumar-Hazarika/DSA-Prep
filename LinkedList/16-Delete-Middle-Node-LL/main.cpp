da75fd605fbef8270814b282888f7911d42fb456
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-06-10 01:41:21 +0530

    Add implementations for deleting the middle node in a linked list using brute force and optimized methods

--- LinkedList/16-Delete-Middle-Node-LL/main.cpp
+++ LinkedList/16-Delete-Middle-Node-LL/main.cpp
@@ -31,9 +31,54 @@ void print(Node* head){
     }
 }
 
+Node* brute(Node* head){
+    //Traverse find N
+    //N/2;
+    //Traverse to N/2 node
+    //middle = temp->next;
+    //temp->next = temp->next->next
+    //delete  middle;
+    if(head == NULL || head->next == NULL) return NULL;
+    Node* temp = head; int n = 0;
+    while(temp != NULL){ //O(N)
+        n++;
+        temp = temp->next;
+       
+    }
+       int res  = floor(n/2);
+        temp = head;
+        while(temp != NULL){ //O(N/2)
+            res--;
+            if(res == 0){
+                Node* middle = temp->next;
+                temp->next = temp->next->next;
+                delete middle;
+                break;
+            }
+            temp = temp->next;
+        }
+        //TC :O(N) +O(N/2)
+        return head;
+}
 
+Node* optimised(Node*head){
+    if(head == NULL || head->next == NULL) return head;
+    //Skip one step of slow that it point to previous
+    Node* slow = head;
+    Node* fast = head;
+    fast = fast->next->next;
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    Node* middle = slow->next;
+    slow->next = slow->next->next;
+    delete middle;
+    return head;
+}
 int main(){
-    vector<int> arr= {1,2,3,4,5}; //output : 3
+    vector<int> arr= {1,3,4,7,1,2,6}; //output : 1,2,3,4,1,2,6
     Node* head  = convertArrayToLL(arr);
-    // print(head);
+    head = optimised(head);
+    print(head);
 }
