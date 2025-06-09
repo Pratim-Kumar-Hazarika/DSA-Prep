8f67cd8b18f8a792e4d0bbda76686cced565966f
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-06-10 01:29:03 +0530

    Add implementation to delete the middle node of a linked list

---
+++ LinkedList/16-Delete-Middle-Node-LL/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>  
+#include <vector> 
+
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+    Node(int val){
+        data = val;
+        next = nullptr;
+    }
+};
+
+Node* convertArrayToLL(vector<int>&arr){
+    Node* head = new Node(arr[0]);
+    Node*temp = head;
+    for(int i = 1; i< arr.size();i++){
+        Node* newNode = new Node(arr[i]);
+        temp->next = newNode;
+        temp = temp->next;
+    }
+    return head;
+}
+void print(Node* head){
+    Node* temp  = head;
+    while(temp != NULL){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+
+int main(){
+    vector<int> arr= {1,2,3,4,5}; //output : 3
+    Node* head  = convertArrayToLL(arr);
+    // print(head);
+}
