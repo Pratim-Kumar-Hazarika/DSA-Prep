2b2549318884afd1807b0650c4e332db84564be4
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-06-09 23:40:39 +0530

    Implement optimized intersection point detection for linked lists

--- LinkedList/12-Find-intersection-point-of-Y-LL/main.cpp
+++ LinkedList/12-Find-intersection-point-of-Y-LL/main.cpp
@@ -43,24 +43,63 @@ Node* bruteForce(Node* head1, Node* head2) {
     return nullptr;
 }
 
+Node* collision(Node* t1, Node*t2 , int d){
+    while(d){
+        d--; //O(N2-N1)
+        t2 = t2->next;
+    }
+    while(t1 != t2){
+        t1= t1->next;  //O(N1)
+        t2 = t2->next;
+    }
+    return t1;
+}
+Node* bestCase(Node*head1, Node* head2){
+    //Traverse both the LL
+    //If one is long one is small go to the difference point of that LL
+    //Start traversing
+    int n1 = 0 , n2=0;
+    Node*temp = head1;
+    while(temp != NULL){  //O(N1)
+        n1++;
+        temp = temp->next;
+    }
+    temp = head2;
+    while(temp != NULL){  //O(N2)
+        n2++;
+        temp = temp->next;
+    }
+    if(n1 < n2){
+        return collision(head1 , head2 , n2-n1);
+    }else{
+        return collision(head2,head1, n1-n2);
+    }
+    return NULL;
+    // TC - O(N1) + O(N2) + O(N2-N1) + O(N1) = O(N + 2N2)
+    // SC - O(1)
+    
+}
+
 int main() {
 
     //Create intersection
-    // 4 → 1 → 
-    //          ↘
-    //            8 → 4 → 5
-    //          ↗
-    // 5 → 6 → 1
+    // 10 → 9 → 4 → 1 → 
+    //                  ↘
+    //                  8 → 4 → 5
+    //                 ↗
+    //        5 → 6 → 1
 
-    // Create intersection: 8 → 4 → 5
+ // Create intersection: 8 → 4 → 5
     Node* intersection = new Node(8);
     intersection->next = new Node(4);
     intersection->next->next = new Node(5);
 
-    // List A: 4 → 1 → 8 → 4 → 5
-    Node* head1 = new Node(4);
-    head1->next = new Node(1);
-    head1->next->next = intersection;
+    // List A: 10 → 9 → 4 → 1 → 8 → 4 → 5
+    Node* head1 = new Node(10);
+    head1->next = new Node(9);
+    head1->next->next = new Node(4);
+    head1->next->next->next = new Node(1);
+    head1->next->next->next->next = intersection;
 
     // List B: 5 → 6 → 1 → 8 → 4 → 5
     Node* head2 = new Node(5);
@@ -68,7 +107,7 @@ int main() {
     head2->next->next = new Node(1);
     head2->next->next->next = intersection;
 
-    Node* result = bruteForce(head1, head2);
+    Node* result = bestCase(head1, head2);
     if (result != nullptr)
         cout << "Intersection point is " << result->data << endl;
     else
