dea73b1dbca2a7b882459fe7e1394ccd557cb05f
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-06-10 23:59:38 +0530

    Add implementation to rotate a linked list to the right by k places

--- LinkedList/22-Rotate-a-LL/main.cpp
+++ LinkedList/22-Rotate-a-LL/main.cpp
@@ -31,9 +31,34 @@ Node * printLL(Node* head) {
     return head;
 }
 
+  Node* rotateRight(Node* head, int k) {
+        if(head == NULL || head->next == NULL || k == 0) return head;
+        int length = 1;
+        Node* curr = head;
+        while(curr->next != NULL){ //O(N)
+               length++;
+             curr = curr->next;
+         
+           
+        }
+        curr->next = head;
+        k = k% length;
+        if(k == 0) {
+            curr->next = NULL;
+            return head;
+        }
+        k = length - k ;
+        while(k--) curr = curr->next; //O(N)
+        head = curr->next;
+        curr->next = NULL;
+        return head;
+        //TC :O(2N)
+        //SC :O(1)
+    }
 int main (){
     vector<int> arr = {1, 2, 3, 4, 5};
     Node* head = convetToLL(arr);
     printLL(head);
    
+
 };
