b074023c2d00b594f0c0f3216d0320c4b428f377
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-06-11 18:47:02 +0530

    Add optimized implementation to merge two sorted linked lists

--- LinkedList/23-Merge-two-sorted-LL/main.cpp
+++ LinkedList/23-Merge-two-sorted-LL/main.cpp
@@ -55,12 +55,37 @@ Node* bruteForce(Node* l1 , Node* l2){
     // SC : O(N) + O(N)
 
 }
+
+Node* optimised(Node* l1 , Node* l2){
+    Node* t1 = l1;
+    Node* t2 = l2;
+    Node* dummyNode = new Node(-1);
+    Node* temp = dummyNode;
+    while(t1 != NULL && t2 != NULL){
+        if(t1->data < t2->data){
+            temp->next = t1;
+            temp = t1;
+            t1 = t1->next;
+        }else{
+            temp->next = t2;
+            temp = t2;
+            t2 = t2->next;
+        }
+    }
+    if(t1){
+        temp->next = t1;
+    }else{
+        temp->next = t2;
+    }
+    return dummyNode->next;
+}
+
 int main(){
     vector<int> list1 = {2,4,8,10};
     vector<int> list2 = {1,3,4,6,11,14};
     Node* head1 = convertToLL(list1);
     Node* head2 = convertToLL(list2);
 
-    Node* merged = bruteForce(head1,head2);
+    Node* merged = optimised(head1,head2);
     printLL(merged);
 }
