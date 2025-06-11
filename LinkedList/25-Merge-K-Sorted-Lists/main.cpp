ca74225477ffcd838d5438b9dc48ac23540ca62f
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-06-11 20:58:03 +0530

    Add optimized implementation to merge K sorted linked lists

--- LinkedList/25-Merge-K-Sorted-Lists/main.cpp
+++ LinkedList/25-Merge-K-Sorted-Lists/main.cpp
@@ -46,6 +46,42 @@ Node* bruteForce(vector<Node*> lists){
     //SC : N * K + M log M + M
     //O  : O(M) ( storing all elements in array)  + O(M) ( new list) 
 }
+Node* merge2Lists(Node* l1 , Node*l2){
+    Node* dummy = new Node(-1);
+    Node* temp = dummy;
+    while(l1 != NULL && l2 != NULL){
+        if(l1->data < l2->data){
+            temp->next = l1;
+            temp = l1;
+            l1 = l1->next;
+        }else{
+            temp->next = l2;
+            temp = l2;
+            l2 = l2->next;
+        }
+    }
+    if(l1) temp->next = l1;
+    else temp->next = l2;
+    return dummy->next;
+}
+Node* optimised(vector<Node*> lists){
+    Node*head = lists[0];
+    for(int i = 1; i< lists.size() ; i++){
+        head = merge2Lists(head, lists[i]);
+    }
+    return head;
+    //TC : (N1+ N2) first list , second list
+    //    (N1 + N2) + N3
+    //    (N1+N2+N3) + N4
+    /// Let's assume all list to be Size N
+    //(N1+ N2) = 2N
+    //(N1 + N2) + N3 = 3N
+    //(N1+N2+N3) + N4 = 4N
+    // N + 2N + 3N + ...KN
+    // N (1 + 2 + 3.. K)
+    // N * (K*K+1)/2 = N^3
+    // O(1)
+}
 int main(){
     vector<int> list1 = {2,4,6};
     vector<int> list2 = {1,5};
@@ -57,6 +93,6 @@ int main(){
     Node* head4 = convertToLL(list4);
     vector<Node*> lists ={head1,head2,head3,head4};
 
-    Node* newNode = bruteForce(lists);
+    Node* newNode = optimised(lists);
     printLL(newNode);
 }
