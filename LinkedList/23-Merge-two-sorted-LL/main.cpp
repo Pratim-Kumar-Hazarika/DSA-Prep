768bbb4415d7665784f0fc80c09b7e404328db39
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-06-11 18:39:58 +0530

    Add brute force implementation to merge two sorted linked lists

--- LinkedList/23-Merge-two-sorted-LL/main.cpp
+++ LinkedList/23-Merge-two-sorted-LL/main.cpp
@@ -31,12 +31,36 @@ Node * printLL(Node* head) {
     return head;
 }
 
+Node* bruteForce(Node* l1 , Node* l2){
+    //Take an array
+    //Store l1 , l2
+    //Sort
+    //Create LL
+    //Return head;
+    vector<int> arr;
+    Node* temp = l1;
+
+    while(temp != NULL){ //O (N)
+        arr.push_back(temp->data);
+        temp = temp->next;
+    }
+    temp = l2;
+    while(temp != NULL){  //O (N)
+        arr.push_back(temp->data);
+        temp = temp->next;
+    }
+    sort(arr.begin(),arr.end());  //O (NLogN)
+    Node* head = convertToLL(arr); //O(N)
+    return head;
+    // SC : O(N) + O(N)
+
+}
 int main(){
     vector<int> list1 = {2,4,8,10};
     vector<int> list2 = {1,3,4,6,11,14};
     Node* head1 = convertToLL(list1);
     Node* head2 = convertToLL(list2);
 
-    // Node* merged = mergedLists(head1,head2);
-    // print(merged);
+    Node* merged = bruteForce(head1,head2);
+    printLL(merged);
 }
