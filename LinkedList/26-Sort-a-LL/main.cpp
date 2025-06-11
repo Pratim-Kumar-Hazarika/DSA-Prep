fac0d9e6cad73cfbd8086a3c7d927835616cdb9f
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-06-11 22:15:47 +0530

    Add optimized implementation for sorting a linked list using merge sort

--- LinkedList/26-Sort-a-LL/main.cpp
+++ LinkedList/26-Sort-a-LL/main.cpp
@@ -45,11 +45,52 @@ Node* brute(Node*head){
     sort(nums.begin(), nums.end()); // N log N
     Node* ans = convertToLL(nums); // O(N)
     return ans;
-    // SC :O N + O N
+    // SC :O N 
 }
+Node* merge(Node* l1 , Node*l2){
+            Node* dummy = new Node(-1);
+            Node* temp = dummy;
+            while(l1 != NULL && l2 != NULL){
+                if(l1->data < l2->data){
+                    temp->next = l1;
+                    temp = l1;
+                    l1 = l1->next;
+                }else{
+                    temp->next = l2;
+                    temp = l2;
+                    l2 = l2->next;
+                }
+            }
+            if(l1) temp->next = l1;
+            else temp->next = l2;
+            return dummy->next;
+    }
+    Node* findMiddle(Node* head){
+        Node* slow = head;
+        Node* fast = head->next; // add fast 1 step so that the middle stops 1 step before ex : 1->2->3->4 : middle will be 2
+        while( fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    Node* optimisedSortList(Node* head) {
+        if(head == NULL  || head->next == NULL) return head;
+        Node*  middle = findMiddle(head); //O(N/2)
+        Node* left = head;
+        Node* right = middle->next;
+        middle->next = NULL;
+        left = optimisedSortList(left);
+        right = optimisedSortList(right);
+        return merge(left,right); //O (N)
+        ///TC :  recursion depth : log N
+        //TC : logN x ( N + N/2)
+        //SC : 1
+
+    }
 int main(){
     vector<int> list = {1,9,4,3,6};
     Node* head = convertToLL(list);
-    Node* ans = brute(head);
+    Node* ans = optimisedSortList(head);
     printLL(ans);
 }
