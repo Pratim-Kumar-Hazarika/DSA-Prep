72d4d7029716d680e62f0a8f6f2ff4c86655e938
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-06-13 16:27:52 +0530

    Add stack implementation using arrays: include push, pop, topElement, and size methods

---
+++ StackAndQueues/1-Implement-Stack-using-arrays/main.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Stack {
+    int st[10]; //TC - O(10) not good
+    int top;
+    public: 
+        Stack(){
+            top = -1;
+        }
+    int topElement(){ //O(1)
+        if(top == -1){
+            return -1;
+        }
+        return st[top];
+    }
+    int size(){  //O(1)
+        return top+1;
+    }
+    int pop(){  //O(1)
+       return st[top--]; 
+    }
+    void push(int val){  //O(1)
+        if (top == 9) {
+            cout << "Stack overflow!" << endl;
+            return;
+        }
+        st[++top] = val;
+
+    }
+};
+
+int main(){
+
+    Stack s;
+    s.push(1);
+    s.push(4);
+   cout << s.topElement() << endl;
+   cout <<  s.size() << endl;
+    return 0;
+}
