8b23ef1f7474790ce98f36f19d49cc9be18809b4
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-06-16 19:59:47 +0530

    Add optimized solution for Next Greater Element II: implement nextGreaterElementsOptmised function

--- StackAndQueues/10-Next-Greater-Element-II/main.cpp
+++ StackAndQueues/10-Next-Greater-Element-II/main.cpp
@@ -18,10 +18,28 @@ vector<int> nextGreaterElementsBrute(vector<int>& nums) {
     }
     return nge;
 }
+vector<int> nextGreaterElementsOptmised(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> nge(n , -1);
+        stack<int> st;
+        for(int i = 2*n-1 ; i >=0 ; i--){ //TC O(2N)
+                 int idx = i % n;
+            while(!st.empty() && st.top() <= nums[idx]){
+                st.pop(); //TC O(2N)
+            }
+            if(!st.empty()){
+                nge[idx] = st.top();
+            }
+            st.push(nums[idx]); //TC O(2N)
+        }
+        //TC :O(4N)
+        //SC :O(2N) stack size + O(N) return the ans
+        return nge;
+    }
 int main(){
     vector<int> nums = {1,2,1};
     ///Output: [2,-1,2]
-    vector<int> ans = nextGreaterElementsBrute(nums);
+    vector<int> ans = nextGreaterElementsOptmised(nums);
     for(int num : ans){
         cout << num << endl;
     }
