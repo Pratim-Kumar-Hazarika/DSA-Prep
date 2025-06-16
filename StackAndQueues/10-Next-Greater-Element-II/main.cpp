f4281ea662f403efb0012b90fb883c60677bb76e
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-06-16 19:45:39 +0530

    Add brute force solution for Next Greater Element II: implement nextGreaterElementsBrute function

--- StackAndQueues/10-Next-Greater-Element-II/main.cpp
+++ StackAndQueues/10-Next-Greater-Element-II/main.cpp
@@ -2,6 +2,28 @@
 #include<stack>
 using namespace std;
 
+vector<int> nextGreaterElementsBrute(vector<int>& nums) {
+    int N = nums.size();
+    vector<int> nge(N , -1);
+    //TC O(N^2)
+    //SC O(N)
+    for(int i = 0 ; i< N ; i++){ //O (N)
+        for(int j = i+1 ; j < i + N ; j++){ //O(N)
+            int ind = j % N ;
+            if( nums[ind] > nums[i]){
+                nge[i] = nums[ind];
+                break;
+            }
+        }
+    }
+    return nge;
+}
 int main(){
+    vector<int> nums = {1,2,1};
+    ///Output: [2,-1,2]
+    vector<int> ans = nextGreaterElementsBrute(nums);
+    for(int num : ans){
+        cout << num << endl;
+    }
     return 0;
 }
