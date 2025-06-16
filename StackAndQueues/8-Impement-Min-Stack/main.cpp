5d79a3deada0dd864ca646a6e153cee953e4f446
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-06-16 16:13:27 +0530

    Add optimized MinStack implementation: include push, pop, top, and getMin methods

--- StackAndQueues/8-Impement-Min-Stack/main.cpp
+++ StackAndQueues/8-Impement-Min-Stack/main.cpp
@@ -34,8 +34,52 @@ public:
         return st.top().second;
     }
 };
+
+
+class MinStackOptimised {
+    //Trick : 2 x val - prev_mini = newVal;
+    stack<long long > st;
+    long long mini = INT_MAX;
+    public:
+        MinStackOptimised(){
+
+        };
+
+        void push(int val){
+            long long x = val;
+            if(st.empty()){
+                mini = x;
+                st.push(x);
+            }else{
+                if(val < mini){
+                    st.push(2*val*1LL-mini);
+                    mini = val;
+                }else{
+                    st.push(val);
+                }
+            }
+        }
+        void pop(){
+            long long el = st.top();
+            st.pop();
+            if(el < mini){
+                mini = 2*mini - el;
+            }
+        };
+        int top(){
+            if(st.empty())return -1;
+            long long el = st.top();
+            if(el < mini){
+                return mini;
+            } return el;
+        };
+        int getMin(){
+            return mini;
+        }
+
+};
 int main(){
-    MinStackBrute st;
+    MinStackOptimised st;
     st.push(5);
     st.push(3);
     st.push(7);
