2ed5622e825f9c2de180d158febba7747670d0ff
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-06-16 17:27:08 +0530

    Fix comment formatting in nextGreaterElementBrute function: correct space notation for space complexity

--- StackAndQueues/9-Next-Greater-Element-I/main.cpp
+++ StackAndQueues/9-Next-Greater-Element-I/main.cpp
@@ -7,7 +7,7 @@ vector<int> nextGreaterElementBrute(vector<int>& nums1, vector<int>& nums2){
     vector<int> nge(n1,-1);
     int n2 = nums2.size();
     //TC : O N^2
-    //SC : O(n1)
+    //SC : O(N1)
     for(int i = 0 ; i< n1 ; i++){
         for(int j = 0 ; j < n2 ; j ++){
             if(nums1[i] == nums2[j]){
