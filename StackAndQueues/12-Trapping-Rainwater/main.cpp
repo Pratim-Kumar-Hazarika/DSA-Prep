c630bbe85747a4f20f6eea349151fafdd439c8c7
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-06-17 22:22:01 +0530

    Add optimized solution for Trapping Rainwater: implement trapOptmises function in main.cpp

--- StackAndQueues/12-Trapping-Rainwater/main.cpp
+++ StackAndQueues/12-Trapping-Rainwater/main.cpp
@@ -32,6 +32,34 @@ using namespace std;
         //SC : P(2N)
     }
 
+ int trapOptmises(vector<int>& arr) {
+        int n = arr.size();
+        if (n == 0) return 0;
+        int left = 0 , right = n-1;
+        int leftMax = 0 , rightMax = 0, total = 0;
+        while(left < right){
+            if(arr[left] <= arr[right]){
+                if(leftMax > arr[left]){
+                    total+= leftMax - arr[left];
+                }else{
+                    leftMax = arr[left];
+                }
+                left++;
+            }else{
+                if(rightMax > arr[right]){
+                    total+= rightMax - arr[right];                    
+                }else{
+                    rightMax = arr[right];
+                }
+                right--;
+            }
+        }
+
+        return total;
+        //TC : O(N)
+        //SC : O(1)
+      
+    }
 int main(){
     vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1};
     //output 6;
