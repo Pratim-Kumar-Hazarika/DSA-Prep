0eb472b52a36cf6e131ee971a4923df2227838c4
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-06-20 16:14:14 +0530

    Add optimized solution for Sliding Window Maximum: implement maxSlidingWindowOptimised function

--- StackAndQueues/19-Sliding-Window-Maximum/main.cpp
+++ StackAndQueues/19-Sliding-Window-Maximum/main.cpp
@@ -19,13 +19,31 @@ vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         //TC : O(N-K)
 }
 
+ vector<int> maxSlidingWindowOptimised(vector<int>& nums, int k) {
+        deque <int> dq;
+        vector<int> ans;
+        int n = nums.size();
+        for(int i = 0 ;i < n ; i++){ //TC O(N)
+            if(!dq.empty() && !dq.front() == i - k){
+                dq.pop_front();
+            }
+            while(!dq.empty() && nums[dq.back()] <= nums[i]){  //TC O(N)
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if(i >= k - 1) ans.push_back(nums[dq.front()]);
+        }
+        return ans;
+        //TC :O (2N)
+        // SC : O(N-K) + O(K)
+    }
 int main(){
     vector<int> nums = {1,3,-1,-3,5,3,6,7};
     //out  [3,3,5,5,6,7]
     int k = 3;
     vector<int> ans = maxSlidingWindow(nums,k);
     for(int num : ans){
-        
+
         cout << num << " ";
     }
     return 0;
