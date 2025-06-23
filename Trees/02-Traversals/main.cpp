1e42b6a7dd8daaf8b6ac9cfc401dc62f507cd5a0
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-06-23 19:09:29 +0530

    Enhance traversal documentation: add detailed comments for DFS and BFS techniques in main.cpp

--- Trees/02-Traversals/main.cpp
+++ Trees/02-Traversals/main.cpp
@@ -1,14 +1,22 @@
 #include<iostream>
 using namespace std;
 
+//         1
+//       /   \
+//      2     3
+//     / \   / \
+//    4   5 6   7
 
 //Two traversals techniques
-//1. DFS  ( Depth First Search)
-//2. BFS  ( Breadth First Search)
+//1. DFS  ( Depth First Search) It travel depth wise
+    //Inorder Traversal (Left Root Right) - >  4 2 5 1 6 3 7
+    //Pre-order Traversal (Root Left Right)- > 1 2 4 5 3 6 7
+    //Post-order Traversal (Left Right Root)-> 4 5 2 6 7 3 1
 
-//Inorder Traversal (Left Root Right)
-//Pre-order Traversal (Root Left Right)
-//Post-order Traversal (Left Right Root)
+
+//2. BFS  ( Breadth First Search)
+//BFS travel level wise - > 1 23 4567
+
 int main(){
     return 0;
 }
