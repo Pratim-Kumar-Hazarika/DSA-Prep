0e864b429043fa860a9d43eeb68ac87929c4bc0d
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-06-23 18:36:24 +0530

    Refactor binary tree representation: move main.cpp to new directory structure and create additional traversal file

---
+++ Trees/02-Traversals/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+//Two traversals techniques
+//1. DFS  ( Depth First Search)
+//2. BFS  ( Breadth First Search)
+
+//Inorder Traversal (Left Root Right)
+//Pre-order Traversal (Root Left Right)
+//Post-order Traversal (Left Right Root)
+int main(){
+    return 0;
+}
