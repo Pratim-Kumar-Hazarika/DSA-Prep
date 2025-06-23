863b8dc47f9d0b0ecfd60d43ae3db1e1fd2d3e96
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-06-23 23:07:10 +0530

    Add post-order traversal implementation: create Node class and main function for binary tree traversal

---
+++ Trees/05-PostOrder/main.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+    Node(int val){
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+
+//         1
+//       /   \
+//      2     3
+//     / \   / \
+//    4   5 6   7
+
+//Post-order Traversal (Left Right Root)- >4 5 2 6 7 3 1
+void postOrder(Node* node){
+    if(node == NULL){
+        return;
+    }
+    postOrder(node->left);
+    postOrder(node->right);
+    cout << node->data << " ";
+
+}
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    postOrder(root);
+    return 0;
+}
