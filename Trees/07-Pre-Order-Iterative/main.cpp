0694ea85d5a97303f5bce32cbd94e2ef33bd6546
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-06-24 16:41:16 +0530

    Add iterative pre-order traversal implementation for binary tree

--- Trees/07-Pre-Order-Iterative/main.cpp
+++ Trees/07-Pre-Order-Iterative/main.cpp
@@ -22,14 +22,25 @@ class Node{
 //    4   5 6   7
 
 //Pre-order Traversal (Root Left Right)- > 1 2 4 5 3 6 7
-void preOrderIterative(Node* node){
-    if(node == NULL){
-        return;
+vector<int> preorderTraversal(Node* root) {
+        vector<int> ans;
+        if(root == NULL) return ans;
+        stack<Node*> st;
+        st.push(root);
+        while(!st.empty()){
+            Node* node = st.top();
+            st.pop();
+            ans.push_back(node->data);
+            if(node->right){
+                st.push(node->right);
+            }
+            if(node->left){
+                st.push(node->left);
+            }
+        }
+        return ans;
     }
-    cout << node->data << " ";
-    preOrderIterative(node->left);
-    preOrderIterative(node->right);
-}
+
 
 int main(){
     Node* root = new Node(1);
@@ -39,6 +50,9 @@ int main(){
     root->left->right = new Node(5);
     root->right->left = new Node(6);
     root->right->right = new Node(7);
-    preOrderIterative(root);
+    vector<int> ans = preorderTraversal(root);
+    for(int nums : ans){
+        cout << nums << " ";
+    }
     return 0;
 }
