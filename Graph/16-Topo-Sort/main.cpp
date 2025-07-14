b01e9e7e81881e3d36b2db88dbab3e6435d91e32
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-07-14 22:16:13 +0530

    Implement BFS solution for topological sorting in a directed graph

--- Graph/16-Topo-Sort/main.cpp
+++ Graph/16-Topo-Sort/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <queue>
 using namespace std;
 
-class Solution {
+class SolutionDFS {
 private:
     void dfs(int start, vector<int>& vis, vector<vector<int>>& edges, stack<int>& st) {
         vis[start] = 1;
@@ -36,21 +37,62 @@ public:
     }
 };
 
-int main() {
-    int V = 4;  // Number of vertices
-    int E = 2;  // Number of edges
+class SolutionBFS {
+public:
+    vector<int> topoSort(int V, vector<vector<int>>& adj) {
+        vector<int> indegree(V, 0);
+        for (int i = 0; i < V; i++) {
+            for (auto it : adj[i]) {
+                indegree[it]++;
+            }
+        }
 
+        queue<int> q;
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        vector<int> topo;
+        while (!q.empty()) {
+            int node = q.front(); q.pop();
+            topo.push_back(node);
+
+            for (auto it : adj[node]) {
+                indegree[it]--;
+                if (indegree[it] == 0) q.push(it);
+            }
+        }
+        return topo;
+    }
+};
+
+int main() {
+    int V = 6;
     vector<vector<int>> edges(V);
 
-    edges[1].push_back(2);
-    edges[1].push_back(0);
+    edges[5].push_back(2);
+    edges[5].push_back(0);
+    edges[4].push_back(0);
+    edges[4].push_back(1);
+    edges[2].push_back(3);
+    edges[3].push_back(1);
 
-    Solution sol;
-    vector<int> topo = sol.topoSort(V, edges);
+    SolutionDFS solDFS;
+    vector<int> topoDFS = solDFS.topoSort(V, edges);
 
+    cout << "Topological Sort (DFS): ";
+    for (int node : topoDFS) {
+        cout << node << " ";
+    }
+    cout << endl;
 
-    cout << "Topological Sort: ";
-    for (int node : topo) {
+    SolutionBFS solBFS;
+    vector<int> topoBFS = solBFS.topoSort(V, edges);
+
+    cout << "Topological Sort (BFS): ";
+    for (int node : topoBFS) {
         cout << node << " ";
     }
     cout << endl;
