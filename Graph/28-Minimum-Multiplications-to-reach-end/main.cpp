c4c6ebdc6aed7f9748db61567e4f3e3653cbb028
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-07-22 23:14:43 +0530

    Implement solution for minimum multiplications to reach the end using BFS

---
+++ Graph/28-Minimum-Multiplications-to-reach-end/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    int minimumMultiplications(vector<int>& arr, int start, int end) {
+        if (start == end) return 0;
+
+        queue<pair<int, int>> q;
+        vector<int> dist(100000, 1e9);
+        q.push({start, 0});
+        dist[start] = 0;
+
+        while (!q.empty()) {
+            int node = q.front().first;
+            int steps = q.front().second;
+            q.pop();
+
+            for (auto it : arr) {
+                int num = (it * node) % 100000;
+                if (steps + 1 < dist[num]) {
+                    dist[num] = steps + 1;
+                    if (num == end) return steps + 1;
+                    q.push({num, steps + 1});
+                }
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> arr = {9, 12, 18, 19};
+    int start = 5;
+    int end = 5;
+
+    int result = sol.minimumMultiplications(arr, start, end);
+  
+
+    return 0;
+}
