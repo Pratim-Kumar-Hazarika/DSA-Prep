040556e4ceee18e016fd7263b6a359f507b2d5ec
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-08-04 21:55:23 +0530

    Add functions to print one subsequence and count subsequences with sum equal to K

--- Recursion/02-Print-Subsequences-whos-sum-is-K/main.cpp
+++ Recursion/02-Print-Subsequences-whos-sum-is-K/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
  using namespace std;
 
+//Print all subsequences of an array whose sum is equal to a given value 's'
  void printf(int ind , vector<int>&ds , int arr[], int n,int s , int curSum){
     if(ind == n){
        if( curSum == s){
@@ -19,10 +20,52 @@
     curSum-= arr[ind];
     printf(ind+1,ds,arr,n,s,curSum);
  }
+ //Just print one subsequence of an array whose sum is equal to a given value 's'
+  bool printf1(int ind , vector<int>&ds , int arr[], int n,int s , int curSum){
+    if(ind == n){
+       if( curSum == s){
+           for(auto it : ds){
+               cout << it << " ";
+           }
+           return true;
+       }
+        // cout << endl;
+        return false;
+    }
+    ds.push_back(arr[ind]);
+    curSum+= arr[ind];
+    if(printf1(ind+1,ds,arr,n,s,curSum) == true) return true;
+    ds.pop_back();
+    curSum-= arr[ind];
+ if(printf1(ind+1,ds,arr,n,s,curSum) == true) return true;
+ return false;
+ }
+ 
+
+ ///count subsequences of an array whose sum is equal to a given value 's'
+ int returnCountOfSubsequence(int ind , int arr[], int n,int s , int curSum){
+    if(curSum > s) return 0; // If current sum exceeds 's', no need to proceed further
+    if(ind == n){
+       if( curSum == s){
+         
+           return 1;
+       }
+        // cout << endl;
+        return 0;
+    }
+    curSum+= arr[ind];
+    int lh = returnCountOfSubsequence(ind+1,arr,n,s,curSum) ;
+    curSum-= arr[ind];
+  int rh = returnCountOfSubsequence(ind+1,arr,n,s,curSum) ;
+ return lh+rh;
+ }
+
  int main(){
      vector<int> ds;
      int arr[] ={1,2,1};
      int sum = 2;
      int n = 3;
     printf(0,ds,arr,n,sum,0);
+    printf1(0,ds,arr,n,sum,0);
+      cout << returnCountOfSubsequence(0,arr,n,sum,0);
  }
