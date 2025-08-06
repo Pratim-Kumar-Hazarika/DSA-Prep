72bd33a7de0ab786da209e8a94df297bbe6704de
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-08-06 23:35:15 +0530

    Update main.cpp

--- Recursion/07-Permutation-1/main.cpp
+++ Recursion/07-Permutation-1/main.cpp
@@ -34,6 +34,27 @@ public:
     }
 };
 
+
+class Solution2 {
+public:
+    void recur(int ind,vector<vector<int>>&ans,vector<int>& nums ){
+        if(ind == nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i = ind ; i < nums.size() ; i++){
+            swap(nums[ind], nums[i]);
+            recur(ind+1,ans,nums);
+            swap(nums[ind], nums[i]);
+        }
+
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        recur(0,ans,nums);
+        return ans;
+    }
+};
 int main() {
     Solution sol;
     vector<int> nums = {1, 2, 3};
