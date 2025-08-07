0740703a541a8f307c6f411e8b69d67ee7d0ab45
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-08-08 00:52:53 +0530

    Implement solution for palindrome partitioning of a string

---
+++ Recursion/10-Palindrome-Partitioning/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>  ans;
+        vector<string> path;
+        func(0, s, path, ans);
+        return ans;
+    }
+
+    void func(int index, string s, vector<string>& path, vector<vector<string>>& ans) {
+        if (index == s.size()) {
+            ans.push_back(path);
+            return;
+        }
+        for (int i = index; i < s.size(); i++) {
+            if (isPalindrome(s, index, i)) {
+                path.push_back(s.substr(index, i - index + 1));
+                func(i + 1, s, path, ans);
+                path.pop_back();
+            }
+        }
+    }
+
+    bool isPalindrome(string s, int start, int end) {
+        while (start <= end) {
+            if (s[start++] != s[end--]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+    string input = "aab";
+    Solution sol;
+    vector<vector<string>> result = sol.partition(input);
+
+    cout << "Palindrome partitions of \"" << input << "\":" << endl;
+    for (const auto& partition : result) {
+        cout << "[ ";
+        for (const auto& part : partition) {
+            cout << "\"" << part << "\" ";
+        }
+        cout << "]" << endl;
+    }
+
+    return 0;
+}
